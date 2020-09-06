5da7097f7fd6d7804d505584e8809d6f134a4f8e
afreelancer1 <56665706+afreelancer1@users.noreply.github.com>
2020-09-06 13:43:35 -0700

    Create changeImage.py

---
+++ changeImage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import os
+images_path = "/home/student-00-9f38004092b9/supplier-data/images"
+
+size = (600,400)
+for i in os.listdir(images_path):
+  if i.endswith("tiff"):
+    im = Image.open(images_path + "/" + i)
+    im = im.resize(size)
+    im = im.convert("RGB")
+    im.save(images_path + "/" + i[:3] + ".jpeg")
