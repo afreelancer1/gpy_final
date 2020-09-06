4061247df45fab44f694e57e86fb9bc72f209ca3
afreelancer1 <56665706+afreelancer1@users.noreply.github.com>
2020-09-06 13:45:14 -0700

    Create example_upload.py

---
+++ example_upload.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import requests
+import os
+
+url = "http://localhost/upload/"
+images_path = "/home/student-00-9f38004092b9/supplier-data/images"
+for i in os.listdir(images_path):
+    if i.endswith("jpeg"):
+        with open(images_path + "/" + i, 'rb') as opened:
+            r = requests.post(url, files={'file': opened})
