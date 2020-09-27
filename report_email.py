e08c591fee92676cb62d68bc24eab8672d11bbb2
afreelancer1 <56665706+afreelancer1@users.noreply.github.com>
2020-09-26 22:13:48 -0700

    Update report_email.py

--- report_email.py
+++ report_email.py
@@ -3,7 +3,8 @@
 import datetime, os
 
 json_path = "/home/student-00-9f38004092b9/supplier-data/descriptions"
-body = "Processed Update on " + str(datetime.datetime.now())
+body = "Processed Update on " + str(datetime.datetime.now()) #adding current time to the string
+
 for i in os.listdir(json_path):
   if i.endswith("txt"): #filtering out the txt files
     with open(json_path + "/" + i, 'r') as f:
