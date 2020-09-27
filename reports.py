e48a0bdd6cdf7bd76137c1dc5106b5acece0a6bb
afreelancer1 <56665706+afreelancer1@users.noreply.github.com>
2020-09-26 22:14:55 -0700

    Update reports.py

--- reports.py
+++ reports.py
@@ -6,16 +6,18 @@ from reportlab.lib.styles import getSampleStyleSheet
 
 def generate_report():
   json_path = "/home/student-00-9f38004092b9/supplier-data/descriptions"
-  rrr = ""
+  body = ""
   report = SimpleDocTemplate("/tmp/processed.pdf")
   styles = getSampleStyleSheet()
+  
   for i in os.listdir(json_path):
     if i.endswith("txt"):
       with open(json_path + "/" + i, 'r') as f:
         ln = f.readlines()
-        rrr += "<br/> <br/> name: " +  ln[0].strip() + "<br/> weight: " + ln[1].strip()
+        body += "<br/> <br/> name: " +  ln[0].strip() + "<br/> weight: " + ln[1].strip()
+        
   report_title = Paragraph("Processed Update on " + str(datetime.datetime.now()), styles["h1"])
-  report_table = Paragraph(rrr, styles["Normal"])
+  report_table = Paragraph(body, styles["Normal"])
   report.build([report_title, report_table])
 
 if __name__ == "__main__":
