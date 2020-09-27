0f3ab150b3bece4224859cd76a494b10212e5265
afreelancer1 <56665706+afreelancer1@users.noreply.github.com>
2020-09-26 22:06:46 -0700

    Update changeImage.py

--- changeImage.py
+++ changeImage.py
@@ -2,11 +2,12 @@
 
 from PIL import Image
 import os
-images_path = "/home/student-00-9f38004092b9/supplier-data/images"
 
+images_path = "/home/student-00-9f38004092b9/supplier-data/images"
 size = (600,400)
+
 for i in os.listdir(images_path):
-  if i.endswith("tiff"):
+  if i.endswith("tiff"): #filtering out the image files
     im = Image.open(images_path + "/" + i)
     im = im.resize(size)
     im = im.convert("RGB")
