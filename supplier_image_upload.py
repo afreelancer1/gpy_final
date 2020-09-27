a2aa14fab199d7c8c6e25ac2ff6eaf51eb02935c
afreelancer1 <56665706+afreelancer1@users.noreply.github.com>
2020-09-26 22:15:38 -0700

    Update supplier_image_upload.py

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -5,6 +5,7 @@ import os
 
 url = "http://localhost/upload/"
 images_path = "/home/student-00-9f38004092b9/supplier-data/images"
+
 for i in os.listdir(images_path):
     if i.endswith("jpeg"):
         with open(images_path + "/" + i, 'rb') as opened:
