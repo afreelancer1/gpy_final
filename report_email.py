15e07f2dbb6d2f15a68b1dd93306e8539d70406a
afreelancer1 <56665706+afreelancer1@users.noreply.github.com>
2020-09-26 22:13:24 -0700

    Update report_email.py

--- report_email.py
+++ report_email.py
@@ -3,11 +3,11 @@
 import datetime, os
 
 json_path = "/home/student-00-9f38004092b9/supplier-data/descriptions"
-bbb = "Processed Update on " + str(datetime.datetime.now())
+body = "Processed Update on " + str(datetime.datetime.now())
 for i in os.listdir(json_path):
-  if i.endswith("txt"):
+  if i.endswith("txt"): #filtering out the txt files
     with open(json_path + "/" + i, 'r') as f:
       ln = f.readlines()
-      bbb += "\n \n name: " +  ln[0].strip() + "\n weight: " + ln[1].strip()
+      body += "\n \n name: " +  ln[0].strip() + "\n weight: " + ln[1].strip() #parsing the txt file
       
-emails.generate_email(bbb)
+emails.generate_email(body)
